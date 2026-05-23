491a6d055c5d5176f3c8f6895ef80692da9fd891
samsulrifai <samsulrifai48@gmail.com>
2026-05-23 19:40:54 +0700

    feat: add Vercel Analytics for page view tracking

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from '@/components/ui/sonner'
 import { TooltipProvider } from '@/components/ui/tooltip'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import './index.css'
 
@@ -22,6 +23,7 @@ createRoot(document.getElementById('root')!).render(
       <BrowserRouter>
         <TooltipProvider>
           <App />
+          <Analytics />
           <Toaster position="top-right" richColors />
         </TooltipProvider>
       </BrowserRouter>
